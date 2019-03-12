a39b85f8383a98a80a2097326364877004427460
Jorge <jbocanegragrandez@gmail.com>
2019-03-11 18:50:57 -0700

    Add Users and Posts routes

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require("body-parser");
+
+//  Routes:
+const users = require("./routes/api/users.js");
+const posts = require("./routes/api/posts");
 
 mongoose
 .connect(db, {useNewUrlParser: true})
 .then(() => console.log("Connected to MongoDB :) "))
 .catch(err => console.log(err));
 
+
+// Express commands
 app.get("/", (req, res) => res.send("Hello World 3"));
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json())
 
 
 const port = process.env.PORT || 5000;
