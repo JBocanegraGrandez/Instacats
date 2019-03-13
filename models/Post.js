2a2406f522340c6347ad568fbf351d064c01c836
Jorge <jbocanegragrandez@gmail.com>
2019-03-12 20:32:55 -0700

    Add Posts models and validations

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+
+    img: {
+        type: String,
+        required: true
+    },
+
+    caption: {
+        type: String,
+        required: false
+    },
+
+    location: {
+        type: String,
+        required: false
+    },
+
+    tags: {
+        type: String,
+        required: false
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Post = mongoose.model('posts', PostSchema);
+module.exports = Post;
---
+++ validation/posts.js
@@ -0,0 +1,22 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validatePostInput(data) {
+    let errors = {}
+
+    data.caption = validText(data.text) ? data.text : '';
+    data.img = validText(data.img) ? data.img : '';
+
+    if (!Validator.isLength(data.caption {min: 0, max: 2200})) {
+        errors.caption = "Caption is too long, maximun is 2200 chars"
+    }
+
+    if (Validator.isEmpty(data.img)) {
+        errors.img = "Could not load the image, please try again"
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    }
+}
