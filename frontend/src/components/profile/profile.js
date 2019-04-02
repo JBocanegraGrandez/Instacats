9f8b93e6c99bf88c2d565d1974ede68f8c4e6f07
Jorge <jbocanegragrandez@gmail.com>
2019-04-02 14:59:22 -0700

    Add Sprite and reworked Profile Details

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PostBox from '../posts/post_box';
+import Pic from '../../profile.jpg'
+import './profile.css'
 
 class Profile extends React.Component {
     constructor(props) {
@@ -21,7 +23,71 @@ class Profile extends React.Component {
 
     render(){
         if (this.state.posts.length === 0) {
-            return (<div>This user has no Posts</div>)
+            return (
+              <div className="Profile-wrapper">
+                <div className="Profile-holder">
+                  <header className="Profile-holder-header">
+                    <div className="Profile-holder-header-picture-wrapper">
+                      <div className="Profile-holder-header-picture-holder">
+                        <div className="Profile-holder-header-picture">
+                          <img className="Profile-pic" src={Pic} />
+                        </div>
+                      </div>
+                    </div>
+                    <section className="Profile-holder-header-details">
+                      <div className="Profile-holder-header-details-main">
+                        <h1>Username</h1>
+                        <a className="Edit-profile">
+                          <button className="Edit-profile-button">
+                            Edit Profile
+                          </button>
+                        </a>
+                        <div />
+                      </div>
+                      <ul className="Profile-holder-header-details-info">
+                        <li className="Profile-stats-info">
+                          <span>
+                            <span className="stats-numbers">Number</span> posts
+                          </span>
+                        </li>
+                        <li className="Profile-stats-info">
+                          <span>
+                            <span className="stats-numbers">Number</span> followers
+                          </span>
+                        </li>
+                        <li className="Profile-stats-info">
+                          <span>
+                            <span className="stats-numbers">Number</span> following
+                          </span>
+                        </li>
+                      </ul>
+                      <div className="Profile-holder-header-details-details">
+                        <h1 className="Full-name">Name, Lastname</h1>
+                        <br/>
+                        <span>Description</span>
+                      </div>
+                    </section>
+                  </header>
+                  <div className="Profile-holder-stories" />
+                  <div className="Profile-holder-menu">
+                    <a className="Profile-holder-menu-item showing">
+                        <span className="Profile-holder-menu-item-span">
+                            <div className="SpritePhotoGridActive"></div>
+                            <span className="Profile-holder-menu-item-text">Posts</span>
+                        </span>
+                    </a>
+                    <a className="Profile-holder-menu-item">
+                        <span className="Profile-holder-menu-item-span">
+                            <div className="SpriteTagged"></div>
+                            <span className="Profile-holder-menu-item-text">Tagged</span>
+                        </span>
+                    </a>
+                  </div>
+                  <div className="Profile-holder-posts" />
+                </div>
+                <div>This user has no Posts</div>
+              </div>
+            );
         } else {
             return (
                 <div>
