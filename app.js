636a6a322a10d16da147cc6b76620bb35ae281ed
Jorge <jbocanegragrandez@gmail.com>
2019-07-02 12:58:07 -0700

    Fix line 50 on app.js

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const passport = require('passport');
 const path = require("path");
 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 
 // Database Schemas
 const User = require('./models/User');
